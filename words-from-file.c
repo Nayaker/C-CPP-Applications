d6d12e9888e6fcba6a5f934d92654bec63f72c2e
Atanu Nayak <93304796+Nayaker@users.noreply.github.com>
2022-11-29 21:15:05 +0530

    Pull words directly from a file
    I used to use fgetc to get words and separate them on space ' '. This is better.

---
+++ words-from-file.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+int main() {
+    FILE* file; 
+    file = fopen("file.txt", "r");
+    if (file == NULL) {
+        printf("no such file.");
+        return 0;
+    }
+ 
+    /* Assuming that file.txt has content in below
+       format
+       NAME    AGE   CITY
+       abc     12    hyderabad
+       bef     25    delhi
+       cce     65    bangalore */
+
+    char city[100];
+    while (fscanf(file, "%*s %*s %s", city) == 1)
+        printf("%s\n", city);
+
+    // %*s %*s %s -> brings three strings from the file, and each time 
+    // stores the third value on city. 
+
+    // If we have %*s %*s %s %*s, we would have got CITY bef 65 instead
+ 
+    return 0;
+}
